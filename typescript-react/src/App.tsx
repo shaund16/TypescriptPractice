d49e7ce15b94530af15198eb2953938a8603f962
Shaun <s-purslow@hotmail.com>
2021-12-22 20:23:42 -0700

    react typescript practice

---
+++ typescript-react/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Counter } from './Counter';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Counter>
+        {(count, setCount) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )}
+      </Counter>
+    </div>
+  );
+};
+
+export default App;
